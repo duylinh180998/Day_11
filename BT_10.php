dc305bba864ab23a423fb8ef5d6a04e34d22005e
duylinh180998 <coldboy98hy@gmail.com>
2019-06-05 08:52:40 +0700

    Day_11

---
+++ BT_10.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<table border="1px" cellspacing="0px" cellpadding="10px">
+    <tr>
+        <th>1</th>
+        <th>2</th>
+        <th>3</th>
+        <th>4</th>
+        <th>5</th>
+    </tr>
+    <tr>
+        <td><?php $a=1;for ($i=1;$i<=10;$i++){echo "$a x $i=".($a*$i); echo "<br/>";} ?></td>
+        <td><?php $a=2;for ($i=1;$i<=10;$i++){echo "$a x $i=".($a*$i); echo "<br/>";} ?></td>
+        <td><?php $a=3;for ($i=1;$i<=10;$i++){echo "$a x $i=".($a*$i); echo "<br/>";} ?></td>
+        <td><?php $a=4;for ($i=1;$i<=10;$i++){echo "$a x $i=".($a*$i); echo "<br/>";} ?></td>
+        <td><?php $a=5;for ($i=1;$i<=10;$i++){echo "$a x $i=".($a*$i); echo "<br/>";} ?></td>
+    </tr>
+    <tr>
+        <th>6</th>
+        <th>7</th>
+        <th>8</th>
+        <th>9</th>
+        <th>10</th>
+    </tr>
+    <tr>
+        <td><?php $a=6;for ($i=1;$i<=10;$i++){echo "$a x $i=".($a*$i); echo "<br/>";} ?></td>
+        <td><?php $a=7;for ($i=1;$i<=10;$i++){echo "$a x $i=".($a*$i); echo "<br/>";} ?></td>
+        <td><?php $a=8;for ($i=1;$i<=10;$i++){echo "$a x $i=".($a*$i); echo "<br/>";} ?></td>
+        <td><?php $a=9;for ($i=1;$i<=10;$i++){echo "$a x $i=".($a*$i); echo "<br/>";} ?></td>
+        <td><?php $a=10;for ($i=1;$i<=10;$i++){echo "$a x $i=".($a*$i); echo "<br/>";} ?></td>
+    </tr>
+</table>
+</body>
+</html>
